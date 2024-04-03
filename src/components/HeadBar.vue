de918aa13a48d9554661e9eded1ef20aa16ac063
needhourger <chainyuuki@gmail.com>
2024-04-03 14:27:16 +0800

    feat: basic header

--- src/components/HeadBar.vue
+++ src/components/HeadBar.vue
@@ -3,11 +3,22 @@
 </script>
 
 <template>
-  <div>
-    head bar
+  <div class="header">
+    <div class="title">Kolibra</div>
   </div>
 </template>
 
 <style scoped>
-
+.header {
+  height: 60px;
+  display: flex;
+  padding: 10px;
+  align-items: center;
+  box-shadow: 0 2px 5px 3px #f0f0f0;
+  .title {
+    font-family: 'Silkscreen';
+    font-size: 42px;
+    font-weight: 600;
+  }
+}
 </style>
