77b4fe422699fc48ba38420a885680c85b3d2230
needhourger <chainyuuki@gmail.com>
2024-04-03 14:58:33 +0800

    feat: basic self

---
+++ src/components/Bookshelf.vue
@@ -0,0 +1,33 @@
+<script setup>
+</script>
+
+<template>
+  <div class="bookshelf">
+    <el-row :gutter="20">
+      <el-col class="book-box" :xs="8" :sm="6" :md="4" :lg="3">
+        <el-card shadow="hover">
+          <template #default>
+            body
+          </template>
+          <template #footer>
+            footer
+          </template>
+        </el-card>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<style scoped lang="less">
+.bookshelf {
+  padding: 20px 40px;
+
+  .book-box {
+    height: 300px;
+    .el-card {
+      height: 100%;
+      width: 100%;
+    }
+  }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
 import Headbar from '@/components/HeadBar.vue'
+import Bookshelf from "@/components/Bookshelf.vue"
 </script>
 
 <template>
   <div>
     <Headbar/>
+    <Bookshelf/>
   </div>
 </template>
