7602f5bb9d54009d1a868f983da1db3d09a92e87
needhourger <chainyuuki@gmail.com>
2024-06-11 16:42:13 +0800

    feat: chapters

--- src/api/index.js
+++ src/api/index.js
@@ -7,3 +7,7 @@ export const getBookByID = (bid) => {
   const url = `/books/${bid}`
   return request.get(url)
 }
+export const getBookChaptersById = (bid) => {
+  const url = `/books/${bid}/chapters`
+  return request.get(url)
+}
--- src/views/BookDetail.vue
+++ src/views/BookDetail.vue
@@ -2,16 +2,26 @@
   <div v-loading="loading" class="px-10 py-5">
     <h1 class="text-3xl font-bold">{{ bookInfo.Title }}</h1>
     <h2 class="">Author: {{ bookInfo.Author }}</h2>
+    <el-divider></el-divider>
+    <h1 class="text-3xl font-bold">Chapters</h1>
+    <el-row :gutter="12" v-if="bookInfo">
+      <el-col :span="8" v-for="chapter, cindex in chapters" :key="cindex">
+        <RouterLink :to="`/${bookInfo.Extension.replace(/^\.*/,'')}/read/${chapter.ID}`">
+          <h2>{{ chapter.Title }}</h2>
+        </RouterLink>
+      </el-col>
+    </el-row>
   </div>
 </template>
 <script setup>
 import { onMounted, ref } from 'vue';
-import { getBookByID } from '@/api'
+import { getBookByID, getBookChaptersById } from '@/api'
 import { ElMessage } from 'element-plus';
 const props = defineProps({
   bookId: { type: String, default: '' },
 })
 const bookInfo = ref({})
+const chapters = ref([])
 const loading = ref(false)
 const queryBookDetail = () => {
   loading.value = true
@@ -26,7 +36,17 @@ const queryBookDetail = () => {
     loading.value = false
   })
 }
+const queryBookChapters = () => {
+  getBookChaptersById(props.bookId).then(res => {
+    if (res) {
+      chapters.value = res
+    }
+  }).then(err => {
+    console.log(err)
+  })
+}
 onMounted(() => {
   queryBookDetail()
+  queryBookChapters()
 })
 </script>
