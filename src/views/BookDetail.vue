6ade41585a5a17dc1e690f70032c3686e838072d
needhourger <chainyuuki@gmail.com>
2024-06-12 18:29:58 +0800

    feat: fix toc layout

--- src/views/BookDetail.vue
+++ src/views/BookDetail.vue
@@ -5,8 +5,9 @@
     <el-divider></el-divider>
     <h1 class="text-3xl font-bold">Chapters</h1>
     <el-row :gutter="12" v-if="bookInfo">
-      <el-col :span="8" v-for="chapter, cindex in chapters" :key="cindex">
-        <RouterLink :to="`/reader/${bookId}/${chapter.ID}`">
+      <el-col :span="8" v-for="chunks, ci in chaptersSplited" :key="ci">
+        <RouterLink  v-for="chapter, cindex in chunks" :key="cindex"
+          :to="`/reader/${bookId}/${chapter.ID}`">
           <h2>{{ chapter.Title }}</h2>
         </RouterLink>
       </el-col>
@@ -14,7 +15,7 @@
   </div>
 </template>
 <script setup>
-import { onMounted, ref } from 'vue';
+import { computed, onMounted, ref } from 'vue';
 import { getBookByID, getBookChaptersById } from '@/api'
 import { ElMessage } from 'element-plus';
 const props = defineProps({
@@ -23,6 +24,10 @@ const props = defineProps({
 const bookInfo = ref({})
 const chapters = ref([])
 const loading = ref(false)
+const chaptersSplited = computed(() => {
+  const chunkSize = Math.ceil(chapters.value.length / 3)
+  return [chapters.value.slice(0,chunkSize), chapters.value.slice(chunkSize+1,2 * chunkSize), chapters.value.slice(2 * chunkSize + 1)]
+})
 const queryBookDetail = () => {
   loading.value = true
   getBookByID(props.bookId).then(res => {
